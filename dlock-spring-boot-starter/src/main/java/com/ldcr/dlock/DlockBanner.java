2f5b7ac1eec15770f4d201ae0180d01ffac81039
boylong12 <boylong08@126.com>
2020-06-05 13:53:31 +0800

    start banner
    Change-Id: Ib7854ebc5bac346da3b01a674304bf19762d51bc

--- dlock-spring-boot-starter/src/main/java/com/ldcr/dlock/DlockBanner.java
+++ dlock-spring-boot-starter/src/main/java/com/ldcr/dlock/DlockBanner.java
@@ -4,6 +4,7 @@ import com.ldcr.dlock.util.DlockVersion;
 import org.springframework.boot.Banner;
 import org.springframework.boot.ansi.AnsiColor;
 import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.ansi.AnsiStyle;
 import org.springframework.core.env.Environment;
 
 import java.io.PrintStream;
@@ -13,21 +14,31 @@ import java.io.PrintStream;
  * @date 2020/6/4 18:01
  */
 public class DlockBanner implements Banner {
-    private static final String BANNER =
-        "   ___   __   ____   _____ __ __       ___   ____   ____  ______      \n" +
-            "  / _ \\ / /  / __ \\ / ___// //_/      / _ ) / __ \\ / __ \\/_  __/  \n" +
-            " / // // /__/ /_/ // /__ / ,<        / _  |/ /_/ // /_/ / / /         \n" +
-            "/____//____/\\____/ \\___//_/|_|      /____/ \\____/ \\____/ /_/      ";
+    private static final String[] BANNER = {
+        "",
+        "   ___   __   ____   _____ __ __       ___   ____   ____  ______",
+        "  / _ \\ / /  / __ \\ / ___// //_/      / _ ) / __ \\ / __ \\/_  __/",
+        " / // // /__/ /_/ // /__ / ,<        / _  |/ /_/ // /_/ / / /   ",
+        "/____//____/\\____/ \\___//_/|_|      /____/ \\____/ \\____/ /_/    "};
 
-    private static final String VERSION_BANNER =
-        "started dlock boot                            (version:" + DlockVersion.getVersion() + ")";
+    private static final String VERSION_BANNER = "dlock boot started";
+
+    private static final int STRAP_LINE_SIZE = 64;
 
     @Override
     public void printBanner(Environment environment, Class<?> sourceClass, PrintStream printStream) {
-        printStream.println();
-        printStream.println(AnsiOutput.toString(AnsiColor.DEFAULT, BANNER));
-        printStream.println();
-        printStream.println(AnsiOutput.toString(AnsiColor.GREEN, VERSION_BANNER));
+        for (String line : BANNER) {
+            printStream.println(line);
+        }
+        String version = DlockVersion.getVersion();
+        version = (version != null) ? " (v" + version + ")" : "";
+        StringBuilder padding = new StringBuilder();
+        while (padding.length() < STRAP_LINE_SIZE - (version.length() + VERSION_BANNER.length())) {
+            padding.append(" ");
+        }
+
+        printStream.println(AnsiOutput.toString(AnsiColor.GREEN, VERSION_BANNER, AnsiColor.DEFAULT, padding.toString(),
+            AnsiStyle.FAINT, version));
         printStream.println();
     }
 }
